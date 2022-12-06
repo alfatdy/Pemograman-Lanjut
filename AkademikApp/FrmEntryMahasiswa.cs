b5a64b7387c1d9a61687c0d55f592b4d77bb52e9
alfatdy <59523082+alfatdy@users.noreply.github.com>
2022-12-06 10:52:27 +0700

    Add files via upload

---
+++ AkademikApp/FrmEntryMahasiswa.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AkademikApp
+{
+    public partial class FrmEntryMahasiswa : Form
+    {
+        public delegate void CreateUpdateEventhandler(Mahasiswa mhs);
+
+        public event CreateUpdateEventhandler OnCreate;
+
+        public event CreateUpdateEventhandler OnUpdate;
+
+        private bool isNewData = true;
+
+        private Mahasiswa mhs;
+
+        // Constructor default
+        public FrmEntryMahasiswa(string title)
+            : this()
+        {
+            this.Text = title;
+        }
+
+        public FrmEntryMahasiswa(string title, Mahasiswa obj)
+            : this()
+        {
+            this.Text = title;
+            isNewData = false;
+            mhs = obj;
+
+            txtNpm.Text = mhs.Npm;
+            txtNama.Text = mhs.Nama;
+            txtAngkatan.Text = mhs.Angkatan;
+        }
+
+        public FrmEntryMahasiswa()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmEntryMahasiswa_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            if (isNewData) mhs = new Mahasiswa();
+
+            mhs.Npm = txtNpm.Text;
+            mhs.Nama = txtNama.Text;
+            mhs.Angkatan = txtAngkatan.Text;
+
+            if (isNewData)
+            {
+                OnCreate(mhs);
+
+                txtNpm.Clear();
+                txtNama.Clear();
+                txtAngkatan.Clear();
+
+                txtNpm.Focus();
+            }
+            else
+            {
+                OnUpdate(mhs);
+                this.Close();
+            }
+
+
+
+        }
+
+        private void btnSelesai_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
